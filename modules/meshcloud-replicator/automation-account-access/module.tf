39affbab6c74c01143ce34d7354228c9848adc17
Mohammad Alhussan <malhussan@meshcloud.io>
2022-03-02 15:36:55 +0100

    chore: identify cloudformation stackset role and policy

--- modules/meshcloud-replicator/automation-account-access/module.tf
+++ modules/meshcloud-replicator/automation-account-access/module.tf
@@ -15,14 +15,16 @@ resource "aws_iam_role_policy_attachment" "meshfed_automation" {
   policy_arn = aws_iam_policy.meshfed_automation.arn
 }
 
+# SSA stands for StackSetAdministration; reduced because of AWS role name limit (64)
 resource "aws_iam_role" "cloudformation_stackset_admin" {
-  name               = "AWSCloudFormationStackSetAdministrationRole"
+  name               = "${var.automation_account_service_role_name}-AWSCloudFormationSSARole"
   description        = ""
   assume_role_policy = data.aws_iam_policy_document.cloudformation_admin_assume_role.json
 }
 
+# SSE stands for StackSetExecution; reduced because of AWS policy name limit (128)
 resource "aws_iam_policy" "cloudformation_stackset_execution" {
-  name   = "AssumeStackSetExecutionRolePolicy"
+  name   = "${var.automation_account_service_role_name}-AssumeSSERolePolicy"
   policy = data.aws_iam_policy_document.cloudformation_stackset_execution.json
 }
 
